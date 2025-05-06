b9ec46a2a42c96b81b51e2d6973c24425f5ff435
MuhdFazril <67180481+fazril-25Java@users.noreply.github.com>
2025-05-06 18:20:27 +0800

    delete+edit database migration

--- database/migrations/2025_04_25_074701_create_bookings_table.php
+++ database/migrations/2025_04_25_074701_create_bookings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('device_id')->constrained('devices')->onDelete('cascade'); // Foreign key to devices table
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Foreign key to users table
-            $table->date('booking_date')->nullable()->change(); // Date when the device is booked
+            $table->date('booking_date')->nullable(); // Date when the device is booked
             $table->date('return_date')->nullable(); // Date when the device is returned
             $table->string('status')->default('booked'); // Status of the booking (e.g., booked, returned, canceled)
             $table->timestamps();
